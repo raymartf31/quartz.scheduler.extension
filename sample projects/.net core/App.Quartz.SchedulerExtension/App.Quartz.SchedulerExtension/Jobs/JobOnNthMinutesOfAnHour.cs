851ca81acd6537f82f8a7220bdaeef9f12da7ad4
Raymart Flores <RaymartF@magenic.com>
2020-06-01 22:39:41 +0800

    .net framework version and .net core sample app.

---
+++ sample projects/.net core/App.Quartz.SchedulerExtension/App.Quartz.SchedulerExtension/Jobs/JobOnNthMinutesOfAnHour.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Quartz;
+
+namespace App.Quartz.SchedulerExtension.Jobs
+{
+    public class JobOnNthMinutesOfAnHour : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            Console.WriteLine($"JobOnNthMinutesOfAnHour executed at: {DateTime.Now}");
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ sample projects/.net core/App.Quartz.SchedulerExtension/App.Quartz.SchedulerExtension/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using App.Quartz.SchedulerExtension.Jobs;
+
+using Quartz.Impl;
+using Quartz.Scheduler.Extension;
+
+namespace App.Quartz.SchedulerExtension
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var scheduler = await StdSchedulerFactory.GetDefaultScheduler();
+
+                var group = "QuartzExtension";
+
+                var schedule = new DateTime(0, 0, 0, 22, 25, 0);
+
+                scheduler.ScheduleJobWithSpecificTime<JobWithSpecificTime>(
+                    group, "Job1", schedule);
+
+                scheduler.ScheduleJobOnNthMinutesOfAnHour<JobOnNthMinutesOfAnHour>(group, "Job2", 2);
+
+                scheduler.ScheduleJobDailyOnNthHourAndNthMinutes<JobDailyOnNthHourAndNthMinutes>(group, "Job3", 22, 30);
+
+                await scheduler.Start();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
